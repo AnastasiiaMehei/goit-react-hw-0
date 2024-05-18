2013c6fb7ce47e95efe8e7639ba13fdc6ebbab17
AnastasiiaMehei <147993093+AnastasiiaMehei@users.noreply.github.com>
2024-05-18 20:48:19 +0300

    add

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,28 +1,48 @@
 import { IoIosSearch } from "react-icons/io";
-import { Formik, Form, Field } from "formik";
-import css from './SearchBar.module.css'
-export default function SearchBar({onSearch}){
-return(
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import css from "./SearchBar.module.css";
+import { toast } from "react-hot-toast";
+
+export default function SearchBar({ onSearch }) {
+  const validationSchema = Yup.object().shape({
+    query: Yup.string()
+      .min(3, "Too Short")
+      .max(50, "Too Long")
+      .required("Required"),
+  });
+
+  return (
     <div className={css.div}>
       <Formik
-    initialValues={{query: ""}}
-    onSubmit={(values, actions)=> {
-      onSearch(values.query)
-      actions.resetForm()}
-    }>
-<Form className={css.form} >
-<div className={css.divButton}>
-<button className={css.btn} type="submit">
-    <IoIosSearch/>
-    </button>
-  <Field className={css.input}
-      type="text"
-      name="query"
-      autoComplete="off"
-      autoFocus
-      placeholder="Search images and photos"/>
-</div>
-</Form>
+        initialValues={{ query: "" }}
+        onSubmit={(values, actions) => {
+          if (values.query.trim() === "") {
+            toast.error("Please enter search text");
+            return;
+          }
+          onSearch(values.query);
+          actions.resetForm();
+        }}
+        validationSchema={validationSchema}
+      >
+        <Form className={css.form}>
+          <div className={css.divButton}>
+            <button className={css.btn} type="submit">
+              <IoIosSearch />
+            </button>
+            <Field
+              className={css.input}
+              type="text"
+              name="query"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+            />
+            <ErrorMessage className={css.span} name="query" component="span" />
+          </div>
+        </Form>
       </Formik>
     </div>
-      )}
+  );
+}
